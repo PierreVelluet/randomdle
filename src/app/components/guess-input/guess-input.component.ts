31b7ff65f24207a2f5b472fdf97f0edb9bc055cf
pvelluet <pvelluer@exelcia-it.com>
2025-04-15 16:40:46 +0400

    input bien fonctionnel

--- src/app/components/guess-input/guess-input.component.ts
+++ src/app/components/guess-input/guess-input.component.ts
@@ -2,13 +2,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Univers } from '../../models/univers.enum';
+import { Keys } from '../../models/keys.enum';
 
 @Component({
-    selector: 'app-guess-input',
-    imports: [CommonModule, FormsModule],
-    templateUrl: './guess-input.component.html',
-    styleUrl: './guess-input.component.scss',
-    standalone: true
+  selector: 'app-guess-input',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './guess-input.component.html',
+  styleUrl: './guess-input.component.scss',
+  standalone: true
 })
 export class GuessInputComponent {
   guess: string = '';
@@ -17,6 +18,7 @@ export class GuessInputComponent {
   @Input() univer: Univers | null = null;
 
   filteredOptions: string[] = [];
+  activeOptionIndex: number = -1;
 
   filterOptions() {
     this.filteredOptions = this.inputOptions.filter(option =>
@@ -38,6 +40,32 @@ export class GuessInputComponent {
       this.guessSubmitted.emit(this.guess.trim());
       this.guess = '';
       this.filteredOptions = [];
+    }
+  }
+
+  handleKeyDown(event: KeyboardEvent): void {
+    switch (event.key) {
+      case Keys.ArrowDown:
+        event.preventDefault();
+        this.activeOptionIndex = (this.activeOptionIndex + 1) % this.filteredOptions.length;
+        break;
+
+      case Keys.ArrowUp:
+        event.preventDefault();
+        this.activeOptionIndex =
+          (this.activeOptionIndex - 1 + this.filteredOptions.length) % this.filteredOptions.length;
+        break;
+
+      case Keys.Enter:
+        if (this.activeOptionIndex >= 0) {
+          this.guess = this.filteredOptions[this.activeOptionIndex];
+          this.filteredOptions = [];
+          this.activeOptionIndex = -1;
+          event.preventDefault();
+        } else {
+          this.submitGuess();
+        }
+        break;
     }
   }
 }
